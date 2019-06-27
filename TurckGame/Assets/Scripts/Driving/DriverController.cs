5b49e5a51fc81a8929a341800557e7e09385fe57
QuickCoyote <36782326+QuickCoyote@users.noreply.github.com>
2019-06-26 18:08:09 -0600

    MOAR TRUCK

---
+++ TurckGame/Assets/Scripts/Driving/DriverController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriverController : MonoBehaviour
+{
+    Suspension suspension = null;
+    Engine engine = null;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ TurckGame/Assets/Scripts/Driving/Engine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour
+{
+    float gearRatio = 1.0f;
+    float horsePower = 100.0f;
+    float torque = 100.0f;
+    float fuelConsumption = 1.0f;
+
+    bool hasEveryPart = true;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!hasEveryPart)
+        {
+            horsePower = 0;
+            torque = 0;
+        }
+    }
+}
